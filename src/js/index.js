38a5994c2c5dbdd1fb315e806f7608170b7fb915
SnYoussef <SnYoussef@AaA.be>
2019-02-14 13:46:30 +0100

    Fin section footer et copyr

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,11 @@ const navbarHid = () => {
     document.body.scrollTop > 600 ||
     document.documentElement.scrollTop > 600
   ) {
-    document.querySelector(".navbar h2").classList.add("d-none");
+    //document.querySelector(".navbar h2").classList.add("d-none"); // titre
     document.querySelector(".navbar-brand").classList.remove("d-none");
     document.querySelector(".navbar").classList.add("fixed-top");
   } else {
-    document.querySelector(".navbar h2").classList.remove("d-none");
+    //document.querySelector(".navbar h2").classList.remove("d-none"); //titre
     document.querySelector(".navbar-brand").classList.add("d-none");
     document.querySelector(".navbar").classList.remove("fixed-top");
   }
